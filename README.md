cefe1a92d183e784fc6e20e77d6c59cfd45af4a7
Roy Williams <roy@anthropic.com>
2024-08-08 17:54:02 -0400

    Implement Zed extension

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # zed-python-refactoring
 Adds refactoring support for Python in Zed
+
+## Installation Steps
+
+### Install the LSP
+
+You can find the LSP implementation at [cst-lsp](https://github.com/rowillia/cst-lsp)
+
+```bash
+pip install cst-lsp
+rustup target add wasm32-wasi
+```
+
+To make sure things are installed properly, run `cst_lsp --stdio`.
+It should run without error (you'll have to Ctrl-C to kill it manually)
+
+
+### Install Development Extension
+
+1. Open Zed
+2. ⌘+Shift+P - "zed: install dev extension"
+3. Point to this directory
+
+If things aren't working check out the logs with:
+
+```bash
+tail -f ~/Library/Logs/Zed/Zed.log
+```
+
+### Add as a language server
+```json
+{
+  "languages": {
+    "Python": {
+      "language_servers": ["pyright", "python_refactoring"]
+    }
+  }
+}
+```
+
+Once installed, you should see a code action to "Refactor" one you select some Python lines.
---
+++ src/python_refactoring.rs
@@ -0,0 +1,48 @@
+use zed_extension_api::{self as zed, settings::LspSettings};
+
+struct PythonRefactoring;
+
+impl zed::Extension for PythonRefactoring {
+    fn new() -> Self {
+        Self {}
+    }
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed_extension_api::LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> zed_extension_api::Result<zed_extension_api::Command> {
+        let path = worktree
+            .which("cst_lsp")
+            .ok_or_else(|| "cst_lsp must be installed and available in $PATH.".to_string())?;
+        Ok(zed::Command {
+            command: path,
+            args: vec!["--stdio".to_string()],
+            env: Default::default(),
+        })
+    }
+    // ref https://github.com/zed-industries/zed/blob/main/extensions/ruff/src/ruff.rs
+    fn language_server_initialization_options(
+        &mut self,
+        language_server_id: &zed_extension_api::LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> zed_extension_api::Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &zed_extension_api::LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> zed_extension_api::Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+}
+
+zed::register_extension!(PythonRefactoring);
